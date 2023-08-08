fc8acfde90a269634ed7f3581b180a3f115b17f1
Shashank Acharya <acharya.s@icloud.com>
2023-08-08 09:35:27 +0800

    feat: add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { PropsWithChildren } from "react"
 import "./globals.css"
 import { DM_Sans } from "next/font/google"
+import NavItems from "./_sections/navigation/NavItems"
+import Logo from "./_components/Logo"
+import SocialButtons from "./_sections/navigation/SocialButtons"
 
 const dmSans = DM_Sans({ subsets: ["latin"] })
 
@@ -8,7 +11,33 @@ export default function RootLayout(props: PropsWithChildren) {
 	const { children } = props
 	return (
 		<html lang="en">
-			<body className={dmSans.className}>{children}</body>
+			<body className={dmSans.className}>
+				<div className="flex w-full justify-center px-8">
+					<div className="flex flex-wrap md:flex-nowrap w-full max-w-4xl md:gap-16">
+						<div className="relative flex items-stretch w-full md:max-w-sm flex-wrap md:flex-nowrap">
+							<div className="flex items-center w-full py-12 md:sticky md:h-screen md:top-0">
+								<div className="w-full">
+									<Logo className="w-full opacity-10 -mb-8" />
+									<div className="flex flex-row md:flex-col gap-8 w-full">
+										<h1 className="md:text-right">
+											Shashank
+											<br />
+											<span className="font-bold text-accent">
+												Acharya
+											</span>
+										</h1>
+										<SocialButtons className="hidden md:flex w-full justify-end" />
+										<NavItems className="w-full items-end justify-center" />
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex items-center w-full py-12">
+							{children}
+						</div>
+					</div>
+				</div>
+			</body>
 		</html>
 	)
 }
