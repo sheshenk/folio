ff6661ed3e67e4f38b6db625cc40a19cb36569d1
Shashank Acharya <acharya.s@icloud.com>
2023-08-08 14:27:39 +0800

    feat: add post detail

---
+++ src/app/_components/PostDetail.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image"
+import Link from "next/link"
+import { PropsWithChildren } from "react"
+
+interface PostDetailProps extends PropsWithChildren {
+	post: Post
+	directory: string
+}
+
+export default function PostDetail(props: PostDetailProps) {
+	const { post, directory, children } = props
+	const { hero_src, hero_alt, title, subtitle, description } = post
+	return (
+		<div className="flex flex-col gap-8">
+			<Link
+				href={directory}
+				className="flex w-fit gap-2 link with-transition"
+			>
+				<p>Back</p>
+			</Link>
+			<Image
+				width={800}
+				height={800}
+				src={hero_src || "/images/test-image.jpeg"}
+				alt={hero_alt || hero_src || ""}
+				className="w-full"
+			/>
+			<div className="flex flex-col">
+				<h2 className="font-bold">{title}</h2>
+				<h3>{subtitle}</h3>
+			</div>
+			{children}
+			<h3 className="border-l-8 pl-4 py-2">
+				<span className="opacity-50">{description}</span>
+			</h3>
+		</div>
+	)
+}
